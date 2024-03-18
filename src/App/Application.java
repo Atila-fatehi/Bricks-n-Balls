d0b69c3c9a0b14846be63e5b3361445b5fec470b
Atila Fatehi <fatehiatila83@gmail.com>
2024-03-18 14:44:50 +0330

    added music

--- src/App/Application.java
+++ src/App/Application.java
@@ -2,8 +2,25 @@ package App;
 
 import Frames.StartPage;
 
-public class Application implements Runnable{
-    public void run(){
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.nio.file.Paths;
+
+public class Application implements Runnable {
+    public void run() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(Paths.get("").toAbsolutePath() + "\\src\\Music\\Theme.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+
         new StartPage();
     }
 }
